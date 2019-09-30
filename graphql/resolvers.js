4df79cce2d7916bf338653e3f176062ae1fd50b7
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-09-30 10:44:30 +0100

    feat: add sort option to users query

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,17 +1,49 @@
 const usersData = require('../data/users');
 
+const sortByUserSort = (userSort) => (a, b) => {
+  const { field, order } = userSort;
+  if (field === 'NAME') {
+    return order === 'ASC' ? a.name.localeCompare(b.name)
+      : b.name.localeCompare(a.name);
+  }
+  if (field === 'AGE') {
+    return order === 'ASC' ? a.age < b.age : b.age < a.age;
+  }
+
+  return true;
+};
+
+function getUsers(first, after, sort) {
+  let users = [...usersData];
+
+  if (sort) {
+    // TODO: sort across all sort options, not just the first
+    users = users.sort(sortByUserSort(sort[0]));
+  }
+
+  if (after) {
+    const afterDate = new Date(after);
+    users = users.filter((u) => new Date(u.created_at) > afterDate);
+  }
+
+  users = users.slice(0, first);
+
+  return users;
+}
+
 const queries = {
-  Users: () => {
-    const data = usersData;
+  Users: (_, { input: { first = 2, after, sort } }) => {
+    const data = getUsers(first, after, sort);
+    const pageInfo = {
+      startCursor: data.length > 0 ? data[0].created_at : null,
+      endCursor: data.length > 0 ? data[data.length - 1].created_at : null,
+    };
     return [{
       edges: data.map((user) => ({
         node: user,
         cursor: user.created_at,
       })),
-      pageInfo: {
-        startCursor: data[0].created_at,
-        endCursor: data[data.length - 1].created_at,
-      },
+      pageInfo,
     }];
   },
 };
