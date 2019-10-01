b8aa46b86167c6912476be0691f5e22803c06a27
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-10-01 15:09:38 +0100

    test: users query

--- app.test.js
+++ app.test.js
@@ -4,13 +4,51 @@ const { start, stop } = require('./app');
 const port = 8081;
 const baseEndpoint = `http://localhost:${port}`;
 
-beforeEach(async () => {
+function createUsersQuery(first = 2, after) {
+  return `
+  {
+    Users(input: {first: ${first}, ${after ? `, after:"${after}"` : ''}}) {
+    
+      edges {
+        node {
+          id
+          name
+          age
+          created_at
+        }
+        cursor
+      }
+      pageInfo {
+        endCursor
+      }
+    }
+  }
+`;
+}
+
+async function findUsers({ first, after = undefined }) {
+  const query = createUsersQuery(first, after);
+  const response = await axios.post(`${baseEndpoint}/graphql`, { query });
+  return response.data.data.Users[0];
+}
+
+beforeAll(async () => {
   await start(port);
 });
 
-afterEach(async () => { await stop(); });
+afterAll(async (done) => { await stop(); done(); });
 
 test('status endpoint reports UP', async () => {
   const { data } = await axios.get(`${baseEndpoint}/status`);
   expect(data).toEqual({ status: 'UP' });
+});
+
+test('return correct number of users', async () => {
+  const users = await findUsers({ first: 3 });
+  expect(users.edges.length).toBe(3);
+});
+
+test('return users after cursor', async () => {
+  const users = await findUsers({ first: 3, after: '1564617600000___age_30_asc' });
+  expect(users.edges.length).toBe(2);
 });
