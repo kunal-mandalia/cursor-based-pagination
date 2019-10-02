3bc7d989a378a8589be0c856fcf86189a94592e4
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-10-02 11:13:30 +0100

    test: sort functionality

--- app.test.js
+++ app.test.js
@@ -4,11 +4,14 @@ const { start, stop } = require('./app');
 const port = 8081;
 const baseEndpoint = `http://0.0.0.0:${port}`;
 
-function createUsersQuery(first = 2, after) {
+
+function createUsersQuery(first = 2, after, sort) {
+  const firstInput = `first:${first}`;
+  const afterInput = after ? `,after:"${after}"` : '';
+  const sortInput = sort ? `,sort: {field:${sort.field},order:${sort.order}}` : '';
   return `
   {
-    Users(input: {first: ${first}, ${after ? `, after:"${after}"` : ''}}) {
-    
+    Users(input: {${firstInput}${afterInput}${sortInput}}) {
       edges {
         node {
           id
@@ -26,18 +29,25 @@ function createUsersQuery(first = 2, after) {
 `;
 }
 
-async function findUsers({ first, after = undefined }) {
-  const query = createUsersQuery(first, after);
+async function findUsers({ first, after = undefined, sort = undefined }) {
+  const query = createUsersQuery(first, after, sort);
   const response = await axios.post(`${baseEndpoint}/graphql`, { query });
-  console.log('findUsers response errors', response.data.errors);
   return response.data.data.Users[0];
+}
+
+async function wait(ms = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 beforeAll(async () => {
   await start(port);
 });
 
-afterAll(async (done) => { await stop(); done(); });
+afterAll(async (done) => {
+  await stop();
+  await wait();
+  done();
+});
 
 test('status endpoint reports UP', async () => {
   const { data } = await axios.get(`${baseEndpoint}/status`);
@@ -49,7 +59,20 @@ test('return correct number of users', async () => {
   expect(users.edges.length).toBe(3);
 });
 
-test('return users after cursor', async () => {
-  const users = await findUsers({ first: 3, after: '1564617600000___age_30_asc' });
-  expect(users.edges.length).toBe(2);
+test('users should be ordered by name ascendingly', async () => {
+  const input = {
+    first: 2,
+    sort: { field: 'name', order: 'asc' },
+  };
+  const users = await findUsers(input);
+  expect(users).toMatchSnapshot();
+});
+
+test('users should be ordered by name descendingly', async () => {
+  const input = {
+    first: 2,
+    sort: { field: 'name', order: 'desc' },
+  };
+  const users = await findUsers(input);
+  expect(users).toMatchSnapshot();
 });
