d8936baff752818be931a6fd59d86e1fd4f6bc95
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2019-10-04 09:46:50 +0100

    refactor: find users query

--- repositories/User.js
+++ repositories/User.js
@@ -1,37 +1,39 @@
-function buildFindUsersQuery(db, Cursor, first, last, before, after, sort) {
-  let cursorInfo;
+function buildFindUsersQuery(db, Cursor) {
+  return async function query(first, last, before, after, sort) {
+    let cursorInfo;
 
-  if (before && after) throw new Error('before and after cursors are mutually exclusive');
+    if (before && after) throw new Error('before and after cursors are mutually exclusive');
 
-  if (before) {
-    cursorInfo = Cursor.deserialize(before, sort);
-  }
-  if (after) {
-    cursorInfo = Cursor.deserialize(after, sort);
-  }
+    if (before) {
+      cursorInfo = Cursor.deserialize(before, sort);
+    }
+    if (after) {
+      cursorInfo = Cursor.deserialize(after, sort);
+    }
 
-  return async () => db.from('app_user')
-    .select('*')
-    .modify((queryBuilder) => {
+    return db.from('app_user')
+      .select('*')
+      .modify((queryBuilder) => {
       //
-      if (cursorInfo) {
-        if (cursorInfo.sort) {
+        if (cursorInfo) {
+          if (cursorInfo.sort) {
+            queryBuilder
+              .orderBy(cursorInfo.sort.field, cursorInfo.sort.order)
+              .where(cursorInfo.sort.field, `${cursorInfo.sort.order === 'asc' ? '>' : '<'}`, cursorInfo.sort.value)
+              .orWhere(cursorInfo.sort.field, '=', cursorInfo.sort.value)
+              .andWhere('created_at', '>', cursorInfo.created_at);
+          } else {
+            queryBuilder
+              .where('created_at', '>', cursorInfo.created_at);
+          }
+        } else if (sort) {
           queryBuilder
-            .orderBy(cursorInfo.sort.field, cursorInfo.sort.order)
-            .where(cursorInfo.sort.field, `${cursorInfo.sort.order === 'asc' ? '>' : '<'}`, cursorInfo.sort.value)
-            .orWhere(cursorInfo.sort.field, '=', cursorInfo.sort.value)
-            .andWhere('created_at', '>', cursorInfo.created_at);
-        } else {
-          queryBuilder
-            .where('created_at', '>', cursorInfo.created_at);
+            .orderBy(sort.field, sort.order);
         }
-      } else if (sort) {
-        queryBuilder
-          .orderBy(sort.field, sort.order);
-      }
-    })
-    .orderBy('created_at', 'asc')
-    .limit(first);
+      })
+      .orderBy('created_at', 'asc')
+      .limit(first);
+  };
 }
 
 class User {
@@ -46,8 +48,8 @@ class User {
   }
 
   async find(first = 2, before, last, after, sort) {
-    const query = await buildFindUsersQuery(this.db, this.Cursor, first, last, before, after, sort);
-    const result = await query();
+    const query = await buildFindUsersQuery(this.db, this.Cursor);
+    const result = await query(first, last, before, after, sort);
     return result;
   }
 }
